596319e644b25554fa136564efead587226005d0
eghosa001 <aighewieghosa111@gmail.com>
2026-09-12 07:46:16 +0100

    db: revoke RPC access to paid-subscription trigger

---
+++ supabase/migrations/20260912073500_revoke_paid_subscription_trigger_rpc.sql
@@ -0,0 +1,3 @@
+revoke all on function public.enforce_paid_subscription_reference() from public;
+revoke execute on function public.enforce_paid_subscription_reference() from anon;
+revoke execute on function public.enforce_paid_subscription_reference() from authenticated;
